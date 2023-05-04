045f6175ea004f4a1af3bc74af599aaca465159b
sushank19 <sushank.sriram19@gmail.com>
2023-05-04 20:02:26 +0530

    commit2

--- weather.js
+++ weather.js
@@ -1,4 +1,3 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
 const options = {
 	method: 'GET',
 	headers: {
@@ -7,33 +6,35 @@ const options = {
 	}
 };
 
-const getweather = (city)=>{
-	cityName.innerHTML = city
-	(async function() {
+const getweather = (city) => {
+	cityName.innerHTML = city;
+	const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+	(async function () {
 		try {
 			const response = await fetch(url, options);
-			const result = await response.text();
+			const result = await response.json();
 			console.log(result);
-			cloud_pct.innerHTML = response.cloud_pct
-			temp.innerHTML = response.temp
-			feels_like.innerHTML = response.feels_like
-			humidity.innerHTML = response.hunidity
-			min_temp.innerHTML = response.min_temp
-			max_temp.innerHTML = response.max_temp
-			wind_speed.innerHTML = response.wind_speed
-			wind_degrees.innerHTML = response.wind_degrees
-			sunrise.innerHTML = response.sunrise
-			sunset.innerHTML = response.sunset
-		} catch (error) {
+			cloud_pct.innerHTML = result.cloud_pct;
+		    temp.innerHTML = result.temp;
+		    feels_like.innerHTML = result.feels_like;
+		    humidity.innerHTML = result.humidity;
+		    min_temp.innerHTML = result.min_temp;
+		    max_temp.innerHTML = result.max_temp;
+		    wind_speed.innerHTML = result.wind_speed;
+		    wind_degrees.innerHTML = result.wind_degrees;
+		    sunrise.innerHTML = result.sunrise;
+		    sunset.innerHTML = result.sunset;
+		} 
+		catch (error) {
 			console.error(error);
-		}		
+		}
 	})();
-}
-
-document.getElementById("submit").addEventListener("submit",(e)=>{
-	e.preventDefault();
-	const city = document.getElementById('city').value;
+};
+  
+document.getElementById("submit").addEventListener("submit", (e) => {
+	e.preventDefault()
+	const city = document.getElementById("city").value;
 	getweather(city);
 });
 
-getweather("Delhi");
+// getweather("new york");
